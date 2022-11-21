7c1148f24c1747bad01d161fd664d695342dba12
kkotula <kkotula13@gmail.com>
2022-11-21 12:02:10 +0100

    Initial commit

---
+++ bgdb/src/main/java/io/armory/bgdb/BgdbApplication.java
@@ -0,0 +1,13 @@
+package io.armory.bgdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgdbApplication.class, args);
+    }
+
+}
